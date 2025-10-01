4d1d3439996884c431ab02197df93160327888f9
IndoorDragon <indoordragoncoco@gmail.com>
2025-10-01 09:53:08 -0400

    Day 8: Find the Index of the First Occurence in a String

---
+++ Day009-FindTheFirstIndexOfTheFirstOccurenceInAString/solution.cpp
@@ -0,0 +1,36 @@
+// Day 9:
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/
+// Date: October 1, 2025
+// Time Complexity: O(n*m)
+// Space Complexity: O(1)
+// Approach: Brute force string matching with nested loops
+// Key Insight: For each position in haystack, check if needle matches starting there.  Use (j-i) to map haystack index to needle index.
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int haystackLength = haystack.size();
+        int needleLength = needle.size();
+        int result = -1;
+
+        for (int i = 0; i < haystackLength; i++) {
+            if (haystack[i] != needle[0]) {
+                continue;
+            }
+            
+            int j;
+            for (j = i; j < haystackLength && (j - i) < needleLength; j++) {
+                if (haystack[j] != needle[j - i]) {
+                    result = -1;
+                    break;
+                }
+            }
+            if ((j - i) == needleLength) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+};
